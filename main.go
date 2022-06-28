e626aeb92c6c89d4eb0cc14bec685c4d59e57a8f
emil-lengman <emil.lengman@schibsted.com>
2022-06-28 02:55:22 +0200

    print attributes

--- main.go
+++ main.go
@@ -18,11 +18,42 @@ func main() {
 	if len(os.Args) < 3 {
 		panic(errors.New("not enough arguments"))
 	}
+
 	if err != nil {
 		panic(err)
 	}
+
 	if os.Args[1] == "text" {
 		getText(bytes, strings.Join(os.Args[2:], " "))
+	}
+
+	if os.Args[1] == "attr" {
+
+		getAttribs(bytes, os.Args[2:])
+	}
+}
+
+func getAttribs(htmlBytes []byte, attributes []string) {
+	reader := bytes.NewReader(htmlBytes)
+	tokenizer := html.NewTokenizer(reader)
+
+	for {
+		tokenType := tokenizer.Next()
+
+		if tokenType == html.ErrorToken {
+
+			return
+		}
+
+		if tokenType == html.StartTagToken || tokenType == html.SelfClosingTagToken {
+			for _, attribute := range tokenizer.Token().Attr {
+				if contains(attributes, attribute.Key) {
+					fmt.Println(attribute.Val)
+				}
+			}
+
+		}
+
 	}
 }
 
