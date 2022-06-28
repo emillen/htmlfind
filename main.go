e39f90e9eb92d7b5c4c72b215d8783d1a501833b
emil-lengman <emil.lengman@schibsted.com>
2022-06-28 04:00:53 +0200

    print comments, and add help text

--- main.go
+++ main.go
@@ -12,25 +12,73 @@ import (
 	"golang.org/x/net/html"
 )
 
+const help = `
+Takes html on stdin, find things in it, and print it to stdout
+
+Usage: htmlfind <command> [<args>]
+
+Commands
+	text <css-selector>	Return the text in the elements returned from the css-selector
+	attr [<attribute>]	Return the value of the attributes
+	comments		Return all HTML comments found in the documents
+`
+
 func main() {
+	file := os.Stdin
+	fi, _ := file.Stat()
+
+	if fi.Size() <= 0 {
+		fmt.Print(help)
+		return
+	}
+
 	bytes, err := ioutil.ReadAll(os.Stdin)
 
-	if len(os.Args) < 3 {
+	if err != nil {
+		panic(errors.New("Failed to read from stdin"))
+	}
+	if len(os.Args) < 2 {
 		panic(errors.New("not enough arguments"))
 	}
 
-	if err != nil {
-		panic(err)
-	}
-
-	if os.Args[1] == "text" {
+	if len(os.Args) >= 3 && os.Args[1] == "text" {
 		getText(bytes, strings.Join(os.Args[2:], " "))
+		return
 	}
 
-	if os.Args[1] == "attr" {
+	if len(os.Args) >= 3 && os.Args[1] == "attr" {
 
 		getAttribs(bytes, os.Args[2:])
+		return
 	}
+
+	if len(os.Args) >= 2 && os.Args[1] == "comments" {
+
+		getComments(bytes)
+		return
+	}
+
+	fmt.Print(help)
+}
+
+func getComments(htmlBytes []byte) {
+	reader := bytes.NewReader(htmlBytes)
+	tokenizer := html.NewTokenizer(reader)
+
+	for {
+		tokenType := tokenizer.Next()
+
+		if tokenType == html.ErrorToken {
+
+			return
+		}
+
+		if tokenType == html.CommentToken {
+			fmt.Println(tokenizer.Token().Data)
+		}
+
+	}
+
 }
 
 func getAttribs(htmlBytes []byte, attributes []string) {
@@ -68,9 +116,11 @@ func getText(htmlBytes []byte, selector string) {
 		panic(err)
 	}
 	for _, ele := range sel.Select(node) {
-		fmt.Println(ele.FirstChild.Data)
+		if ele.FirstChild != nil {
+
+			fmt.Println(ele.FirstChild.Data)
+		}
 	}
-	fmt.Println()
 }
 
 func contains(stringSlice []string, s string) bool {
